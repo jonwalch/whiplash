83452e5bddb4afc65f04b53584fe51be080f8e37
Paul Shryock <paul@pshry.com>
2019-11-01 09:53:03 -0400

    Update footer contact email address
    https://github.com/jonwalch/whiplash/pull/19#pullrequestreview-310252792

--- resources/public/js/components/Home.tsx
+++ resources/public/js/components/Home.tsx
@@ -146,7 +146,7 @@ export function Home(props: any) {
     </main>
     <footer role="contentinfo" className="site-footer">
       <p className="copyright">&copy; Whiplash. All Rights Reserved.</p>
-      <p><strong>Need help?</strong> Contact us at <a href="mailto:help@whiplashesports.com" target="_blank" rel="noreferrer">help@whiplashesports.com</a></p>
+      <p><strong>Need help?</strong> Contact us at <a href="mailto:support@whiplashesports.com" target="_blank" rel="noreferrer">support@whiplashesports.com</a></p>
     </footer>
   );
 }
