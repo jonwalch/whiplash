8ef71e731aa78a18eb1ca1e4cb6827e670e263d2
jonwalch <jonwalch@gmail.com>
2019-11-04 20:46:34 -0800

    Make SES better

--- src/clj/whiplash/integrations/amazon_ses.clj
+++ src/clj/whiplash/integrations/amazon_ses.clj
@@ -1,22 +1,34 @@
 (ns whiplash.integrations.amazon-ses
-  (:require [postal.core :as postal]))
+  (:require [postal.core :as postal]
+            [whiplash.config :refer [env]]
+            [clojure.tools.logging :as log]))
 
 (def aws-ses-config
-  {:user "AKIAUJ3FEDZ5VOZNAGEU"
-   :pass "BENLETvJ/OVvLIOdQjG/veYBsd3aLBDatLiEQ4UlXfYM"
+  {:user "AKIAUJ3FEDZ5XK6Z7THM"
+   :pass "BLF7jQ5tdWxsCTapjL/RZI6h25Ki2UNIqBhI4Mw2bnWX"
    :host "email-smtp.us-west-2.amazonaws.com"
-   :port 25
+   :port 587
    :tls true})
 
 (defn- internal-send-verification-email
   [{:keys [user/email body subject]}]
   ;; On success save this email information to the user
-  (postal/send-message aws-ses-config
-                       {:from    "noreply@whiplashesports.com"
-                        :to      "jonwalch@gmail.com"       ;;email
-                        :cc      nil
-                        :subject subject
-                        :body    body}))
+  (if (:prod env)
+    (future
+      (try (postal/send-message aws-ses-config
+                                {:from    "Whiplash <noreply@whiplashesports.com>"
+                                 :to      email
+                                 :cc      nil
+                                 :subject subject
+                                 :body    body})
+           (catch Throwable t
+             (log/error (format "Failed to send verification email to %s" email) t))))
+    (log/info {:to      email
+               :subject subject
+               :body    body})))
+
+(comment
+  (internal-send-verification-email {:user/email "jonwalch@gmail.com" :subject "hi" :body "hi"}))
 
 (defn send-verification-email
   [{:user/keys [email first-name verify-token] :as user}]
