03f2072e56d8b99980fa40e9432d49a0044b2321
jonwalch <jonwalch@gmail.com>
2019-12-28 16:10:59 -0800

    more visual tweaks

--- resources/public/js/components/Home.tsx
+++ resources/public/js/components/Home.tsx
@@ -88,74 +88,70 @@ export function Home(props: any) {
     // Loading
     if (streamURL == "") {
       return (
-        <div className="twitch is-inactive">
-          <div className="container">
-            <h2 className="twitch__title">Loading...</h2>
-            <div className="twitch__placeholder">
-              <div className="container">
+          <div className="twitch is-inactive">
+            <div className="container">
+              <h2 className="twitch__title">Loading...</h2>
+              <div className="twitch__placeholder">
                 <p className="twitch__subtitle">Hang tight, your CS:GO match is loading.</p>
               </div>
             </div>
           </div>
-        </div>
       );
-    // No stream to show
+      // No stream to show
     } else if (streamURL == failedToFetch) {
       return (
-        <div className="twitch is-inactive">
-          <div className="container">
-            <h2 className="twitch__title">Whiplash is taking a nap</h2>
-            <div className="twitch__placeholder">
-              <div className="container">
+          <div className="twitch is-inactive">
+            <div className="container">
+              <h2 className="twitch__title">Whiplash is taking a nap</h2>
+              <div className="twitch__placeholder">
                 <p className="twitch__subtitle">Hang tight, we'll find a CS:GO match for you soon.</p>
                 <p>In the meantime, bookmark this page and check back often for new chances to win while watching.</p>
               </div>
             </div>
           </div>
-        </div>
       );
-    // Found stream
+      // Found stream
     } else {
       return (
-        <>
-          <div className={"twitch" + (!chatIsOpen ? " chat-is-closed" : "")}>
-            <header className="container twitch__header">
-              <h2 className="twitch__title">{matchName}</h2>
-              <button
-                className="button twitch__button"
-                type="button"
-                onClick={() => {
-                  setChatIsOpen(!chatIsOpen)
-                }}>
-                {chatIsOpen ? 'Close Chat' : 'Open Chat'}
-              </button>
-            </header>
-            <div className="aspect-ratio-wide twitch__video">
-              <iframe
-                src={"https://player.twitch.tv/?channel=" + twitchUsername}
-                frameBorder="0"
-                allowFullScreen={true}>
-              </iframe>
-            </div>
-            {chatIsOpen && <div className="twitch__chat">
+          <>
+            <div className={"twitch" + (!chatIsOpen ? " chat-is-closed" : "")}>
+              <header className="container twitch__header">
+                <h2 className="twitch__title">{matchName}</h2>
+                <button
+                    className="button twitch__button"
+                    type="button"
+                    onClick={() => {
+                      setChatIsOpen(!chatIsOpen)
+                    }}>
+                  {chatIsOpen ? 'Close Chat' : 'Open Chat'}
+                </button>
+              </header>
+              <div className="aspect-ratio-wide twitch__video">
                 <iframe
-                  frameBorder="0"
-                  scrolling="true"
-                  src={"https://www.twitch.tv/embed/" + twitchUsername + "/chat?darkpopout"}>
+                    src={"https://player.twitch.tv/?channel=" + twitchUsername}
+                    frameBorder="0"
+                    allowFullScreen={true}>
                 </iframe>
               </div>
-            }
-          </div>
-          <Vote
-            opponents={opponents}
-            team={team}
-            setTeam={setTeam}
-            matchID={matchID}
-            matchName={matchName}
-            currentGame={currentGame}
-            isProduction={isProduction}
-          />
-        </>
+              {chatIsOpen && <div className="twitch__chat">
+                <iframe
+                    frameBorder="0"
+                    scrolling="true"
+                    src={"https://www.twitch.tv/embed/" + twitchUsername + "/chat?darkpopout"}>
+                </iframe>
+              </div>
+              }
+            </div>
+            <Vote
+                opponents={opponents}
+                team={team}
+                setTeam={setTeam}
+                matchID={matchID}
+                matchName={matchName}
+                currentGame={currentGame}
+                isProduction={isProduction}
+            />
+          </>
       );
     }
   };
