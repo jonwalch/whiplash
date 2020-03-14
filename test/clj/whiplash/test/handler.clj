e8b0f85b870a5033d2336f69dbc7ba412a320b00
jonwalch <jonwalch@gmail.com>
2020-03-13 18:00:23 -0700

    bad event source test

--- test/clj/whiplash/test/handler.clj
+++ test/clj/whiplash/test/handler.clj
@@ -1189,3 +1189,15 @@
                    :stream-source "event.stream-source/youtube"
                    :channel-id    youtube-channel-id}
            (dissoc get-response-body :event/start-time)))))
+
+(deftest bad-event-source
+  (testing "can't create event because source is invalid"
+    (let [{:keys [auth-token] login-resp :response} (create-user-and-login
+                                                      (assoc dummy-user :admin? true))
+          title "Dirty Dan's Delirious Dirty Dancing Watch Party"
+          youtube-channel-id "x135bZ6G"
+          resp (admin-create-event {:auth-token auth-token
+                                    :title      title
+                                    :channel-id youtube-channel-id
+                                    :source "blarp"
+                                    :status 400})])))
