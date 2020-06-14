927a7a9bcc3a5e64308c1deb217c0a607cdd6251
jonwalch <jonwalch@gmail.com>
2020-06-13 22:15:36 -0700

    Fix on brave browser

--- src/js/common/stream.tsx
+++ src/js/common/stream.tsx
@@ -7,7 +7,7 @@ export const getEvent = async () => {
     // @ts-ignore "i dont care if this is null"
     const bundleContentHash = Array.from(document.scripts).filter(
         // @ts-ignore "i dont care if this is unknown"
-        script => script.outerHTML.includes("app"))[0].outerHTML.match(bundleRegex)[1];
+        script => script.outerHTML.includes("/dist/app"))[0].outerHTML.match(bundleRegex)[1];
 
     const response = await fetch(baseUrl + "stream/event", {
         method: "GET",
